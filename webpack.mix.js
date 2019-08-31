d7223f5e7ea5d9fceeee448d5dca79aac9b6a0e5
Ajit Bohra <ajit@lubus.in>
2019-08-31 18:20:46 +0530

    Optimize tailwind build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,12 @@
 const mix = require('laravel-mix');
+require('mix-tailwindcss');
+require('laravel-mix-purgecss');
 
-mix.postCss('src/css/style.css', 'assets/css', [
-    require('tailwindcss'),
-]);
+mix.postCss('src/css/tailwind.css', 'assets/css')
+   .tailwind();
+
+if (mix.inProduction()) {
+	mix.purgeCss({
+		folders: ['includes'],
+	});
+}
