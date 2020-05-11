2f9ff27defc29ba039a09fed330c78bd1784020a
Naveen Kharwar <naveen919827@gmail.com>
2020-05-11 13:57:51 +0530

    improve file and folder structure

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,5 @@ const plugins = [require("tailwindcss")];
 
 mix
     .postCss("src/css/tailwind.css", "assets/css", plugins)
-    .copy('src/js/**/*', 'assets')
+    .copy('src/js/', 'assets/js/')
     .copy('src/css/munim.css', 'assets/css');
