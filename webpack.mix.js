d3493081a8598b9b24d3938e8203607ee3ae8dab
Naveen Kharwar <naveen919827@gmail.com>
2020-05-19 14:27:30 +0530

    JavaScript files minify and clipboard.min.js as NPM

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,7 @@ const plugins = [require("tailwindcss")];
 
 mix
     .postCss("src/css/tailwind.css", "assets/css", plugins)
-    .copy('src/js/', 'assets/js/')
-    .copy('src/css/munim.css', 'assets/css');
+    .copy('src/js/dashboard.js', 'assets/js/dashboard.js').minify('assets/js/dashboard.js')
+    .copy('src/js/script.js', 'assets/js/script.js').minify('assets/js/script.js')
+    .copy('src/css/munim.css', 'assets/css')
+    .copy('node_modules/clipboard/dist/clipboard.min.js', 'assets/js');
