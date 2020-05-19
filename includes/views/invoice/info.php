95cac6c51bcad5c0af4b38581106743a35ffc216
Naveen Kharwar <naveen919827@gmail.com>
2020-05-19 13:07:48 +0530

    tailwind prefix apply to /invoice/

--- includes/views/invoice/info.php
+++ includes/views/invoice/info.php
@@ -15,28 +15,28 @@ global $post;
 $munim_currency_symbol = get_munim_currency_symbol();
 ?>
 <div>
-	<div class="flex flex-col ">
-		<div class="w-full flex">
-			<span class="w-1/2 text-gray-500">
+	<div class="tw-flex tw-flex-col ">
+		<div class="tw-w-full tw-flex">
+			<span class="tw-w-1/2 tw-text-gray-500">
 				<?php esc_html_e( 'Subtotal', 'munim' ); ?>
 			</span>
-			<span class="w-1/2 text-right">
+			<span class="tw-w-1/2 tw-text-right">
 				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_invoice_subtotal ); ?>
 			</span>
 		</div>
-		<div class="w-full flex mb-2">
-			<span class="w-1/2 text-gray-500">
+		<div class="tw-w-full tw-flex tw-mb-2">
+			<span class="tw-w-1/2 tw-text-gray-500">
 				<?php esc_html_e( 'Taxes', 'munim' ); ?>
 			</span>
-			<span class="w-1/2 text-right">
+			<span class="tw-w-1/2 tw-text-right">
 				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_invoice_taxes_total ); ?>
 			</span>
 		</div>
-		<div class="w-full flex border-t border-gray-300 pt-2">
-			<span class="w-1/2 text-gray-500">
+		<div class="tw-w-full tw-flex tw-border-t tw-border-gray-300 tw-pt-2">
+			<span class="tw-w-1/2 tw-text-gray-500">
 				<?php esc_html_e( 'Total', 'munim' ); ?>
 			</span>
-			<span class="w-1/2 text-right">
+			<span class="tw-w-1/2 tw-text-right">
 				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_invoice_total ); ?>
 			</span>
 		</div>
