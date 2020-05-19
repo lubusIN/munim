b26b69bab7af8bdb9ec5f8694d3d61f61f28f54f
Naveen Kharwar <naveen919827@gmail.com>
2020-05-19 16:11:41 +0530

    Minify file structure improved

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,15 @@ const mix = require('laravel-mix');
 
 const plugins = [require("tailwindcss")];
 
+const js_assets = [
+    'assets/js/dashboard.js',
+    'assets/js/script.js',
+    'assets/js/clipboard.js'
+];
+
 mix
     .postCss("src/css/tailwind.css", "assets/css", plugins)
-    .copy('src/js/dashboard.js', 'assets/js/dashboard.js').minify('assets/js/dashboard.js')
-    .copy('src/js/script.js', 'assets/js/script.js').minify('assets/js/script.js')
     .copy('src/css/munim.css', 'assets/css')
-    .copy('node_modules/clipboard/dist/clipboard.min.js', 'assets/js');
+    .copy('node_modules/clipboard/dist/clipboard.js', 'src/js')
+    .copyDirectory('src/js', 'assets/js/')
+    .minify(js_assets);
