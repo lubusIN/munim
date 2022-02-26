1def85605ebe83eeb27b164a7c90167269a22aed
Ajit Bohra <ajit@lubus.in>
2022-02-26 16:50:38 +0530

    Fix php warnings on quick info meta box

--- includes/views/estimate/info.php
+++ includes/views/estimate/info.php
@@ -21,7 +21,7 @@ $munim_currency_symbol = get_munim_currency_symbol();
 				<?php esc_html_e( 'Subtotal', 'munim' ); ?>
 			</span>
 			<span class="tw-w-1/2 tw-text-right">
-				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_subtotal ); ?>
+				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_subtotal ?? 0 ); ?>
 			</span>
 		</div>
 		<div class="tw-w-full tw-flex tw-mb-2">
@@ -29,7 +29,7 @@ $munim_currency_symbol = get_munim_currency_symbol();
 				<?php esc_html_e( 'Taxes', 'munim' ); ?>
 			</span>
 			<span class="tw-w-1/2 tw-text-right">
-				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_taxes_total ); ?>
+				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_taxes_total ?? 0 ); ?>
 			</span>
 		</div>
 		<div class="tw-w-full tw-flex tw-border-t tw-border-gray-300 tw-pt-2">
@@ -37,7 +37,7 @@ $munim_currency_symbol = get_munim_currency_symbol();
 				<?php esc_html_e( 'Total', 'munim' ); ?>
 			</span>
 			<span class="tw-w-1/2 tw-text-right">
-				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_total ); ?>
+				<?php echo esc_html( $munim_currency_symbol ); ?> <?php echo number_format( $post->munim_estimate_total ?? 0 ); ?>
 			</span>
 		</div>
 	</div>
